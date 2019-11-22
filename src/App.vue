d8a286c95a0b041cc26917ba3b67ab369066e2fd
Alex <a.fontana@explora-biotech.com>
2019-11-22 11:56:22 +0100

    add terminator, aptamer, Protein, CDS, RBS, Promoter glyphs

--- src/App.vue
+++ src/App.vue
@@ -27,13 +27,13 @@ export default {
               "public": true,
               "obsolete": false,
               "annotations":[
-                  {"pk":22354,"start":195,"end":219,"direction":1,"name":"F24 primer bind","notes":"","role_id":29},
-                  {"pk":31032,"start":226,"end":259,"direction":1,"name":"lacZa*_18_interval_1","notes":"","role_id":37},
-                  {"pk":31034,"start":259,"end":306,"direction":1,"name":"MIIs B2 (accc)","notes":"","role_id":27},
-                  {"pk":31036,"start":361,"end":652,"direction":1,"name":"lacZa*_18_interval_2","notes":"","role_id":37},
-                  {"pk":31037,"start":613,"end":616,"direction":1,"name":"Premature STOP","notes":"","role_id":14},
-                  {"pk":31038,"start":422,"end":446,"direction":2,"name":"R24 primer bind","notes":"","role_id":29},
-                  {"pk":31039,"start":312,"end":361,"direction":1,"name":"MIIs C1 (gcga)","notes":"Insertion","role_id":27}
+                  {"pk":22354,"start":195,"end":219,"direction":1,"name":"F24 primer bind","notes":"","role_id":29}, //primer oligos
+                  {"pk":31032,"start":226,"end":259,"direction":1,"name":"lacZa*_18_interval_1","notes":"","role_id":37}, //Gene
+                  {"pk":31034,"start":259,"end":306,"direction":1,"name":"MIIs B2 (accc)","notes":"","role_id":27}, //Misc.feature/Other/Unknown
+                  {"pk":31036,"start":361,"end":652,"direction":1,"name":"lacZa*_18_interval_2","notes":"","role_id":37}, //Gene
+                  {"pk":31037,"start":613,"end":616,"direction":1,"name":"Premature STOP","notes":"","role_id":14}, //STOP Codon
+                  {"pk":31038,"start":422,"end":446,"direction":2,"name":"R24 primer bind","notes":"","role_id":29},  //primer oligos
+                  {"pk":31039,"start":312,"end":361,"direction":1,"name":"MIIs C1 (gcga)","notes":"Insertion","role_id":27} //Misc.feature/Other/Unknown
               ]
           }; 
           this.source = jsonFile
--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -14,9 +14,122 @@ export default {
         }
     },
     methods: {
-        render(val) {
-            window.console.log(val);
-            this.editor = d3.select("svg");
+        render() {
+            window.console.log("ooo");
+            
+            //const d = data;
+            this.editor = d3
+                .select("svg")
+                //.attr("width", "100%")
+                .attr("height", 200);
+
+            this.editor.append("defs")
+                .append("marker")
+				.attr("id", "arrow")
+				.attr("viewBox","0 -5 10 10")
+				.attr("refX",5)
+				.attr("refY",0)
+				.attr("markerWidth",4)
+				.attr("markerHeight",4)
+				.attr("orient","auto")
+				.append("path")
+					.attr("d", "M0,-5L10,0L0,5")
+					.attr("class","arrowHead");
+
+            this.editor.append("line")
+                .attr("x1", 10)
+                .attr("y1", 100)
+                .attr("x2", 500)
+                .attr("y2", 100)     
+                .attr("stroke", "#000")
+                .attr("stroke-width", 3)
+
+            this.editor.append("path")
+                .attr('transform', 'translate(20, 100)')
+                .attr("d", "M0,0 L0,-35 L30,-35")
+                .attr("stroke", "#000")
+                .attr("stroke-width", 2.5)
+                .attr("fill", "none")
+                .attr("marker-end","url(#arrow)")
+                .on("click", () => {
+                    alert("ciao! Promoter");
+                })
+
+            const arc = d3.arc();
+            this.editor.append("path")
+                .attr('transform', 'translate(70, 100)')
+                .attr('d', arc({
+                    innerRadius: 0,
+                    outerRadius: 20,
+                    startAngle: -Math.PI*0.5,
+                    endAngle: Math.PI*0.5
+                }))
+                .attr("fill", "#B5A0C7")
+                .attr("stroke", "#000")
+                .attr("stroke-width", 2)
+                .on("click", () => {
+                    alert("ciao! RBS");
+                })
+
+           this.editor.append("path")
+                .attr('transform', 'translate(110, 100)')
+                .attr('d', "M0,0 L0,-12 L70,-12 L80,0 L70,12 L0,12 Z")
+                .attr("opacity", 1)
+                .attr("fill", "#C54E46")
+                .attr("stroke", "#000")
+                .attr("stroke-width", 2)
+                .on("click", () => {
+                    alert("ciao! CDS");
+                })                
+
+            this.appendTerminator()
+            this.appendAptamer()
+            this.appendProtein()
+           
+        },
+        appendTerminator() {
+            let terminator = this.editor.append("g")
+                .attr('transform', 'translate(200, 100)')
+
+            terminator.append("path")
+                .attr("d", "M15,0 L15,-35 L0,-35 L30,-35 L15,-35") 
+                .attr("stroke", "#000")
+                .attr("stroke-width", 3)    
+                .on("click", () => {
+                    alert("ciao! Terminator");
+                })
+        },
+        appendAptamer() {
+            this.editor.append("path")
+                .attr('transform', 'translate(250, 100)')
+                .attr("d","M 0,0 0,-11.5668 c 3.25316,-0.83762 5.65964,-3.78365 5.65964,-7.29791 0,-0.76821 -0.11621,-1.50849 -0.32901,-2.20651 l 6.84439,-3.95194 c 0.53125,0.28676 1.12967,0.46334 1.77637,0.46334 2.08426,0 3.7731,-1.68884 3.7731,-3.7731 0,-2.0835 -1.68884,-3.77309 -3.7731,-3.77309 -2.08501,0 -3.77309,1.68959 -3.77309,3.77309 0,0.034 0.009,0.0664 0.0106,0.0996 l -6.83609,3.94591 c -1.35756,-1.31304 -3.2026,-2.1235 -5.24082,-2.1235 -4.16625,0 -7.54619,3.37843 -7.54619,7.54619 0,3.51501 2.40648,6.46029 5.65964,7.29791 l 0,11.5668")
+                .attr("fill", "#fff")
+                .attr("stroke", "#000")
+                .attr("stroke-width", 3)    
+                .on("click", () => {
+                    alert("ciao! Aptamer");
+                })
+        },
+        appendProtein() {
+            let protein = this.editor.append("g")
+                .attr('transform', 'translate(270, 100)')
+
+            protein.append("circle")                
+                .attr("cx", "22.499998")
+                .attr("cy","-27.489317")
+                .attr("r", "7")
+                .attr("fill", "#fff")
+                .attr("stroke", "#000")
+                .attr("stroke-width", 3)    
+
+            protein.append("path")                
+                .attr("d","M 21.683565,-20.375099 c 8.864406,-1.922677 9.102925,7.881981 -0.15538,6.409716 9.140445,-2.515454 8.800654,8.057991 -0.08776,6.338974 9.06355,-2.471877 8.98889,7.812118 -0.07466,6.239105")
+                .attr("fill", "#fff")
+                .attr("stroke", "#000")
+                .attr("stroke-width", 3)    
+                .on("click", () => {
+                    alert("ciao! Protein");
+                })
         }
     },
     watch: {
