55ba20ffe05e2ab3981a83eba2858209dc0b783e
mamato <m.amato@explora-biotech.com>
2020-07-29 16:23:14 +0200

    SBOL-27

--- src/views/Viewer.vue
+++ src/views/Viewer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="SbolVisualizer">
+  <div class="SbolVisualizer" ref="sbolVisualizer">
     <div v-if="empty === true" class="empty">
       <a href="https://sbolstandard.org/">
         <sbol-logo />
@@ -21,6 +21,9 @@
         <sbol-detail :annotation="annotation" />
       </div>
     </div>
+    <pre class="slot-wrapper hide">
+      <slot></slot>
+    </pre>
   </div>
 </template>
 
@@ -35,7 +38,7 @@ import SbolDetail from "../components/SbolDetail";
 import SbolLogo from "../components/SbolLogo";
 
 export default {
-  props: ["source"],
+  props: ["source", "format", "data"],
   data() {
     return {
       header: {},
@@ -51,9 +54,56 @@ export default {
       window.console.log("showDetail from viewer", data);
       this.annotation = this.annotations[data];
     },
-    matchWidth() {
-      const main = this.$refs.test;
-      this.mainWidth = main.clientWidth + "px";
+    // is this used somewhere???
+    // matchWidth: function() {
+    //   const main = this.$refs.test;
+    //   this.mainWidth = main.clientWidth + "px";
+    // },
+    genericLoad: function(dataFormat, data) {
+      if (dataFormat == "json") {
+        if (typeof data == "string") {
+          data = JSON.parse(data);
+        }
+        this.loadJson(data);
+      }
+      if (dataFormat == "xml") {
+        this.loadXml(data);
+      }
+    },
+    loadJson: function(json) {
+      const jsonName = json.name || "";
+      const jsonFrindlyID = json.friendly_id || "";
+
+      this.header = {
+        partID: jsonFrindlyID,
+        name: jsonName,
+        alternativeName: "",
+        version: "",
+        division: "",
+        parentSequence: ""
+      };
+      this.annotations = json.annotations || [];
+      this.annotations.map(a => {
+        if (a.direction) {
+          a.direction = a.direction === 1 ? "FW" : "RV";
+        } else {
+          a.direction = "--";
+        }
+      });
+      this.empty = false;
+      this.$nextTick(function() {
+        this.chartsWidth = this.$refs.chartsContainer.clientWidth - 50;
+        if (this.chartsWidth < 300) {
+          this.$refs.sbolVisualizer.className = "SbolVisualizer Mini";
+        }
+        // console.log(`this.chartsWidth ${this.chartsWidth}`);
+        // console.log(
+        //   `this.$refs.sbolVisualizer.clientWidth ${this.$refs.sbolVisualizer.clientWidth}`
+        // );
+      });
+    },
+    loadXml: function(xml) {
+      console.log(xml);
     }
   },
   components: {
@@ -64,47 +114,36 @@ export default {
     SbolLogo
   },
   mounted: function() {
-    axios.get(this.source).then(ret => {
-      this.header = {
-        partID: ret.data.friendly_id,
-        name: ret.data.name,
-        alternativeName: "",
-        version: "",
-        division: "",
-        parentSequence: ""
-      };
-      this.annotations = ret.data.annotations;
-      this.annotations.map(a => {
-        if (a.direction) {
-          a.direction = a.direction === 1 ? "FW" : "RV";
-        } else {
-          a.direction = "--";
-        }
+    if (this.format) {
+      // Inline data
+      const dataFormat = this.format == "json" ? "json" : "xml";
+      this.genericLoad(dataFormat, this.data);
+    } else if (this.source) {
+      // Load a file
+      const dataFormat = this.source.indexOf(".json") != -1 ? "json" : "xml";
+      axios.get(this.source).then(data => {
+        this.genericLoad(dataFormat, data.data);
       });
-      this.empty = false;
-
-      this.$nextTick(function() {
-        this.chartsWidth = this.$refs.chartsContainer.clientWidth - 50;
-      });
-    });
+    }
   }
 };
 </script>
 
 <style scoped>
-#SbolVisualizer {
+.SbolVisualizer {
   font-family: Helvetica;
   overflow: scroll;
-
   font-size: 0.875rem;
   font-weight: 400;
   line-height: 1.5;
   color: #202832;
   text-align: left;
   margin: 0;
-  padding: 0;
+  padding: 10px;
 }
-
+.hide {
+  display: none;
+}
 .empty {
   background-color: #f0f2f5;
   border: 1px solid #aaa;
