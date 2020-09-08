8f8eff59e328491c0e8257d9181f79820f070a68
M amato <m.amato@explora-biotech.com>
2020-09-08 13:06:48 +0200

    add  examples

--- src/lib/importer/xmlHandler.js
+++ src/lib/importer/xmlHandler.js
@@ -25,8 +25,13 @@ const xmlHandler = {
         const ids = []
         for (let i = 0; i < dnaComponents.length; i++) {
             const component = dnaComponents[i];
+            // <sbol:definition rdf:resource="https://synbiohub.org/public/igem/BBa_B0034/1"/>
+            //    -->  <sbol:ComponentDefinition rdf:about="https://synbiohub.org/public/igem/BBa_I20282/1">
+            //
+            const sboldetails = xmlHandler.xmlFind_startWith(component, "sbol:definition", "rdf:resource", "https://synbiohub.org/public/igem/");
 
             const role = xmlHandler.xmlFind_startWith(component, "sbol:role", "rdf:resource", "http://identifiers.org/so/SO:");
+
             const sbolIndex = xmlHandler.xmlFind(component, "sbol:displayId");
             if (!ids.includes(sbolIndex)) {
                 ids.push(sbolIndex)
@@ -110,8 +115,58 @@ const xmlHandler = {
             }
         }
     },
+    testElement: (parsedElement, attribute) => {
+
+        if (
+            Object.prototype.toString.call(parsedElement) == "[object HTMLCollection]"
+        ) {
+            parsedElement = parsedElement[0];
+        }
+
+
+        if (typeof attribute === "undefined") {
+            if (
+                typeof parsedElement === "undefined" ||
+                typeof parsedElement.childNodes[0] === "undefined"
+            ) {
+                return false
+            } else {
+                return true
+            }
+        } else {
+
+            if (typeof parsedElement != 'undefined' && parsedElement.hasAttribute(attribute)) {
+                return true
+            } else {
+                return false
+            }
+        }
+    },
     xmlFind_startWith: (ParseXml, elementTagName, attribute, startWith) => {
         return xmlHandler.xmlFindAll_startWith(ParseXml, elementTagName, attribute, startWith)[0]
+    },
+    xmlFindAllElement_startWith: (ParseXml, elementTagName, attribute, startWith) => {
+
+        const allDOMelement = ParseXml.getElementsByTagName(elementTagName);
+        const result = []
+
+        for (let i = 0; i < allDOMelement.length; i++) {
+            const component = allDOMelement[i];
+            const proposed = xmlHandler.xmlFallback(
+                component,
+                "",
+                attribute
+            );
+            if (proposed.startsWith(startWith)) {
+                result.push(component)
+            }
+
+
+        }
+        return result
+    },
+    xmlFindElement_startWith: (ParseXml, elementTagName, attribute, startWith) => {
+        return xmlHandler.xmlFindAllElement_startWith(ParseXml, elementTagName, attribute, startWith)[0]
     },
     xmlFindAll_startWith: (ParseXml, elementTagName, attribute, startWith) => {
 
