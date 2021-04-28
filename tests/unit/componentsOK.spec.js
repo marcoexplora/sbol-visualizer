1dab724323afa9764087e6fbef8cea1742a08c88
m.amato@explora-biotech.com <Calimero1983>
2021-04-28 16:03:41 +0200

    SBOL-105 #workon incresa code coverage

--- tests/unit/componentsOK.spec.js
+++ tests/unit/componentsOK.spec.js
@@ -14,41 +14,100 @@
 
 import { shallowMount } from '@vue/test-utils'
 
-import SbolLogo from '@/components/SbolLogo.vue'
 import SbolHeader from "@/components/SbolHeader.vue";
 import SbolListAnnotations from "@/components/SbolListAnnotations.vue";
+import SbolTreeList from "@/components/SbolTreeList.vue"
 import SbolChart from "@/components/SbolChart.vue";
 import SbolDetail from "@/components/SbolDetail.vue";
 
+import mocked_props from "./mockup_props/PropsListAnnotations"
 
 describe('SbolHeader.vue', () => {
-  it('Is Header component working?', () => {
 
-    const _header = {
-      partID: 'part_id',
-      name: 'name',
-      alternativeName: 'alternativeName',
-      version: 'version',
-      division: 'division',
-      parentSequence: 'parentSequence'
+  const _header = {
+    partID: '$part_id$',
+    name: 'name',
+    alternativeName: 'alternativeName',
+    version: '$version$',
+    division: 'division',
+    parentSequence: 'parentSequence',
+    mutableDescription : '$mutableDescription$',
+    creator : '$creator$'
+  }
+  const wrapper = shallowMount(SbolHeader, {
+    propsData: {
+      header: _header
     }
-
-    const wrapper = shallowMount(SbolHeader, {
-      propsData: {
-        header: _header
-      }
-    })
-    expect(wrapper.html()).toContain('class="heading"')
   })
+
+  it('Header: Is Header component working?', () => {
+    expect(wrapper.html()).toContain('class="mutableDescription')
+  })
+
+  it('Header: Show version?', () => {
+    expect(wrapper.html()).toContain('$version$')
+  })
+
+
+  it('Header: Show part id?', () => {
+    expect(wrapper.html()).toContain('$part_id$')
+  })
+
+
+  it('Header: Show mutableDescription?', () => {
+    expect(wrapper.html()).toContain('$mutableDescription$')
+  })
+
+
+  it('Header: Show creator?', () => {
+    expect(wrapper.html()).toContain('$creator$')
+  })
+
 })
 
-describe('SbolLogo.vue', () => {
-  it('Is SVG SBol LOGO working?', () => {
+describe('SbolListAnnotations.vue - tree closed', () => {
 
-    const wrapper = shallowMount(SbolLogo, {
-      propsData: {}
-    })
-
-    expect(wrapper.html()).toContain('div id="sbolLogo"')
+  const wrapper = shallowMount(SbolListAnnotations, {
+    propsData: mocked_props.initial_state
   })
-})
+
+  console.log(wrapper.html())
+
+  it('SbolListAnnotations: Show Search ?', () => {
+    expect(wrapper.html()).toContain('class="search-input"')
+  })
+
+  it('SbolListAnnotations: Show SbolTreeList ?', () => {
+    expect(wrapper.findAllComponents(SbolTreeList).at(0).html()).toContain('item=')
+  })
+
+  it('SbolListAnnotations: Show SbolTreeList (two components) ?', () => {
+    expect(wrapper.findAllComponents(SbolTreeList).at(1).html()).toContain('item=')
+  })
+
+});
+
+
+
+describe('SbolListAnnotations.vue - tree explored', () => {
+  const _propsData = mocked_props.initial_state;
+  _propsData.breadcrumbs = mocked_props.tree_open.breadcrumbs;
+  _propsData.selected = mocked_props.tree_open.selected;
+
+  const wrapper = shallowMount(SbolListAnnotations, {
+    propsData: _propsData
+  })
+
+  it('SbolListAnnotations: Show Search ?', () => {
+    expect(wrapper.html()).toContain('class="search-input"')
+  })
+
+  it('SbolListAnnotations: Show SbolTreeList ?', () => {
+    expect(wrapper.findAllComponents(SbolTreeList).at(0).html()).toContain('item=')
+  })
+
+  it('SbolListAnnotations: Show SbolTreeList (two components) ?', () => {
+    expect(wrapper.findAllComponents(SbolTreeList).at(1).html()).toContain('item=')
+  })
+
+});
